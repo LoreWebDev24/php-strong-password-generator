028fe4381e6d5eb571916fede1d24623ca0b68c2
Lorenzo Del Re <136103234+LoreWebDev24@users.noreply.github.com>
2023-11-23 15:54:55 +0100

    add the password Generator

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <?php 
+function getUserPassword($charLength) {
+    $passwordChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'];
 
-$charLength = intval($_GET['pass_chr_number']);
+    $paswordGenerating = '';
+    $passwordCharsLength = count($passwordChars);
+    var_dump(count($passwordChars));
+
+    // for ($i = 0, $res = ''; $i < $charLength; $res .= $passwordChars[rand(0, $passwordCharsLength - 1)], $i++) {
+    //     ;
+    // }
+    do {
+        $index = rand(0, $passwordCharsLength - 1);
+        $paswordGenerating .= $passwordChars[$index];
+        var_dump ($paswordGenerating);
+    }
+    while (strlen($paswordGenerating) < $charLength);
+    // return $res;
+    return $paswordGenerating;
+};
+
+
+$charLengthStr = $_GET['pass_chr_number'];
+
+if (!isset($charLengthStr) || $charLengthStr === '') {
+    $charLength = 10;
+} else {
+    $charLength = intval($charLengthStr);
+}
+
 var_dump($charLength);
-
-$passwordChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'];
-
-
 
 ?>
 
@@ -22,5 +45,10 @@ $passwordChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
         <input type="number" name="pass_chr_number" id="pass_chr_number" min="6" max="32">
         <button type="submit">Generate</button>
     </form>
+    <div class="div">
+        <p>
+            <?php echo getUserPassword($charLength) ?>
+        </p>
+    </div>
 </body>
 </html>
